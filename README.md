bb3cf9473a26f5b4e338570e0c40c797d2ccb42d
Ayan Biswas <ayan_biswas@eecs.berkeley.edu>
2020-09-23 16:54:03 -0700

    various submodules and path updates

--- README.md
+++ README.md
@@ -4,7 +4,8 @@
 1. ssh to a server that supports RHEL7 (any one of the bwrcr740-x servers)
 2. add this to your home .bashrc so that devtoolset is enabled everytime a new terminal is opened: `source scl_source enable devtoolset-8 rh-git29 httpd24`
 3. Start bash
-4. Clone this repository
+4. Clone this repository and `cd` into it
+5. `git submodule update --init --recursive`
 5. Create a new cds.lib as follows: `echo "INCLUDE cds.lib.core" >> cds.lib`
 6. `source .bashrc`
 7. `cd BAG_framework/pybag`
--- setup_script.sh
+++ setup_script.sh
@@ -23,10 +23,10 @@ fi
 
 
 # setup symlink to compiled pybag
-#mkdir -p BAG_framework/pybag/_build/lib
-#cd BAG_framework/pybag/_build/lib
-#ln -s ../../../../bag3d0_rhel60_64/pybag .
-#cd ../../../../
+mkdir -p BAG_framework/pybag/_build/lib
+cd BAG_framework/pybag/_build/lib
+ln -s ${BAG_TOOLS_ROOT}/pybag .
+cd ../../../../
 
 # setup symlink for files
 ln -s cds_ff_mpt/workspace_setup/bag_config.yaml bag_config.yaml
@@ -45,46 +45,47 @@ ln -s cds_ff_mpt/workspace_setup/pvtech.lib pvtech.lib
 ln -s BAG_framework/run_scripts/start_bag_ICADV12d3.il start_bag.il
 ln -s BAG_framework/run_scripts/virt_server.sh virt_server.sh
 ln -s BAG_framework/run_scripts/run_bag.sh run_bag.sh
+ln -s BAG_framework/run_scripts/start_bag.sh start_bag.sh
 
 # setup cadence shared library linkage
-#mkdir cadence_libs
-#
-#declare -a lib_arr=("libblosc.so"
-#                    "libblosc.so.1"
-#                    "libblosc.so.1.11.4"
-#                    "libcdsCommon_sh.so"
-#                    "libcdsenvutil.so"
-#                    "libcdsenvxml.so"
-#                    "libcla_sh.so"
-#                    "libcls_sh.so"
-#                    "libdataReg_sh.so"
-#                    "libddbase_sh.so"
-#                    "libdrlLog.so"
-#                    "libfastt_sh.so"
-#                    "libgcc_s.so"
-#                    "libgcc_s.so.1"
-#                    "liblz4.so"
-#                    "liblz4.so.1"
-#                    "liblz4.so.1.7.1"
-#                    "libnffr.so"
-#                    "libnmp_sh.so"
-#                    "libnsys.so"
-#                    "libpsf.so"
-#                    "libsrr_fsdb.so"
-#                    "libsrr.so"
-#                    "libstdc++.so"
-#                    "libstdc++.so.5"
-#                    "libstdc++.so.5.0.7"
-#                    "libstdc++.so.6"
-#                    "libstdc++.so.6.0.22"
-#                    "libvirtuos_sh.so"
-#                    "libz_sh.so"
-#                   )
-#
-#for libname in "${lib_arr[@]}"; do
-#    fpath=${CDS_INST_DIR}/tools/lib/64bit/${libname}
-#    if [ ! -f "$fpath" ]; then
-#        echo "WARNING: Cannot find packaged Virtuoso shared library ${fpath}; symlink will be broken."
-#    fi
-#    ln -s ${fpath} cadence_libs/${libname}
-#done
+mkdir cadence_libs
+
+declare -a lib_arr=("libblosc.so"
+                    "libblosc.so.1"
+                    "libblosc.so.1.11.4"
+                    "libcdsCommon_sh.so"
+                    "libcdsenvutil.so"
+                    "libcdsenvxml.so"
+                    "libcla_sh.so"
+                    "libcls_sh.so"
+                    "libdataReg_sh.so"
+                    "libddbase_sh.so"
+                    "libdrlLog.so"
+                    "libfastt_sh.so"
+                    "libgcc_s.so"
+                    "libgcc_s.so.1"
+                    "liblz4.so"
+                    "liblz4.so.1"
+                    "liblz4.so.1.7.1"
+                    "libnffr.so"
+                    "libnmp_sh.so"
+                    "libnsys.so"
+                    "libpsf.so"
+                    "libsrr_fsdb.so"
+                    "libsrr.so"
+                    "libstdc++.so"
+                    "libstdc++.so.5"
+                    "libstdc++.so.5.0.7"
+                    "libstdc++.so.6"
+                    "libstdc++.so.6.0.22"
+                    "libvirtuos_sh.so"
+                    "libz_sh.so"
+                   )
+
+for libname in "${lib_arr[@]}"; do
+    fpath=${CDS_INST_DIR}/tools/lib/64bit/${libname}
+    if [ ! -f "$fpath" ]; then
+        echo "WARNING: Cannot find packaged Virtuoso shared library ${fpath}; symlink will be broken."
+    fi
+    ln -s ${fpath} cadence_libs/${libname}
+done
